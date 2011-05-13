0f69fc3369873930e32de00f6a722939548c20f6
pfrancois(fixe) <pfrancois_99@yahoo.fr>
2011-05-13 23:57:19 +0200

    coorection bug template tag gsb extra

--- gsb/templatetags/gsb_extras.py
+++ gsb/templatetags/gsb_extras.py
@@ -5,17 +5,18 @@ from decimal import Decimal, InvalidOperation, ROUND_HALF_UP
 from django.utils import formats
 from django.utils.encoding import force_unicode
 from django.utils.safestring import mark_safe
+import logging
 
 register = template.Library()
 
 
 @register.filter()
 def cur(value, symbol="&#8364;"):
+    logger=logging.getLogger('gsb.cur_tag')
     pos_inf = 1e200 * 1e200
     neg_inf = -1e200 * 1e200
     nan = (1e200 * 1e200) / (1e200 * 1e200)
     special_floats = [str(pos_inf), str(neg_inf), str(nan)]
-
     try:
         input_val = force_unicode(value)
         d = Decimal(input_val)
@@ -29,28 +30,7 @@ def cur(value, symbol="&#8364;"):
         except (ValueError, InvalidOperation, TypeError, UnicodeEncodeError):
             return u''
     p = int(2)
-    try:
-        m = int(d) - d
-    except (ValueError, OverflowError, InvalidOperation):
-        return input_val
-    if p:
-        exp = Decimal(1)
-    else:
-        exp = Decimal(u'1.0') / (Decimal(10) ** abs(p))
-    try:
-        # Avoid conversion to scientific notation by accessing `sign`, `digits`
-        # and `exponent` from `Decimal.as_tuple()` directly.
-        sign, digits, exponent = d.quantize(exp, ROUND_HALF_UP).as_tuple()
-        digits = [unicode(digit) for digit in reversed(digits)]
-        while len(digits) <= abs(exponent):
-            digits.append(u'0')
-        digits.insert(-exponent, u'.')
-        if sign:
-            digits.append(u'-')
-        number = u''.join(reversed(digits))
-        return mark_safe("%s %s" % (formats.number_format(number, abs(p)), symbol))
-    except InvalidOperation:
-        return input_val
+    return mark_safe("%s %s" % (formats.number_format(d, abs(p)), symbol))
 cur.is_safe = True
 
 @register.filter()
