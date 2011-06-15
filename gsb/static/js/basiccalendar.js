126a3286c5718a2d42a8b477399f4eab0746f12c
pfrancois(fixe) <pfrancois_99@yahoo.fr>
2011-06-15 02:52:36 +0200

    calendrier fonctionne

--- gsb/static/js/basiccalendar.js
+++ gsb/static/js/basiccalendar.js
@@ -1,8 +1,88 @@
 /* coding: utf-8 */
+var _DATE_FORMAT_REGXES = {
+    'Y': new RegExp('^-?[0-9]+'),
+    'd': new RegExp('^[0-9]{1,2}'),
+    'm': new RegExp('^[0-9]{1,2}'),
+    'H': new RegExp('^[0-9]{1,2}'),
+    'M': new RegExp('^[0-9]{1,2}')
+}
 
+/*
+ * _parseData does the actual parsing job needed by `strptime`
+ */
+function _parseDate(datestring, format) {
+    var parsed = {};
+    for (var i1=0,i2=0;i1<format.length;i1++,i2++) {
+    var c1 = format[i1];
+    var c2 = datestring[i2];
+    if (c1 == '%') {
+        c1 = format[++i1];
+        var data = _DATE_FORMAT_REGXES[c1].exec(datestring.substring(i2));
+        if (!data.length) {
+            return null;
+        }
+        data = data[0];
+        i2 += data.length-1;
+        var value = parseInt(data, 10);
+        if (isNaN(value)) {
+            return null;
+        }
+        parsed[c1] = value;
+        continue;
+    }
+    if (c1 != c2) {
+        return null;
+    }
+    }
+    return parsed;
+}
+
+/*
+ * basic implementation of strptime. The only recognized formats
+ * defined in _DATE_FORMAT_REGEXES (i.e. %Y, %d, %m, %H, %M)
+ */
+function strptime(datestring, format) {
+    var parsed = _parseDate(datestring, format);
+    if (!parsed) {
+    return null;
+    }
+    // create initial date (!!! year=0 means 1900 !!!)
+    var date = new Date(0, 0, 1, 0, 0);
+    date.setFullYear(0); // reset to year 0
+    if (parsed.Y) {
+    date.setFullYear(parsed.Y);
+    }
+    if (parsed.m) {
+    if (parsed.m < 1 || parsed.m > 12) {
+        return null;
+    }
+    // !!! month indexes start at 0 in javascript !!!
+    date.setMonth(parsed.m - 1);
+    }
+    if (parsed.d) {
+    if (parsed.m < 1 || parsed.m > 31) {
+        return null;
+    }
+    date.setDate(parsed.d);
+    }
+    if (parsed.H) {
+    if (parsed.H < 0 || parsed.H > 23) {
+        return null;
+    }
+    date.setHours(parsed.H);
+    }
+    if (parsed.M) {
+    if (parsed.M < 0 || parsed.M > 59) {
+        return null;
+    }
+    date.setMinutes(parsed.M);
+    }
+    return date;
+}
 var currentEditField = "";
 var editorOriginalValue = "";
 var currentEditor = "";
+var format_date="";
 
 function findPosY(obj) {
     var o = obj;
@@ -136,12 +216,13 @@ function buildCal(d, m, y, cM, cH, cDW, cD, brdr) {
 }
 
 
-function editDate(fieldId) {
+function editDate(fieldId,format) {
     var todaydate = new Date();
     var posy;
     var curyear;
     var curmonth;
     var curday;
+    format_date = format;
     if (currentEditor !== "") {
         closeCurrentEditor();
     }
@@ -151,10 +232,11 @@ function editDate(fieldId) {
         posy = findPosY(currentEditField);
         editorOriginalValue = currentEditField.value;
         window.scroll(0, posy);
-        curyear = editorOriginalValue.substring(4, 8) * 1;
-        curmonth = editorOriginalValue.substring(2, 4) * 1;
-        curday = editorOriginalValue.substring(0, 2) * 1;
-        if (editorOriginalValue.length < 8) {
+        myDate =strptime(editorOriginalValue,format)
+        curyear = myDate.getFullYear * 1;
+        curmonth = myDate.getMonth * 1;
+        curday = myDate.getDay * 1;
+        if (editorOriginalValue.length != 8) {
             curyear = todaydate.getFullYear() * 1;
             curmonth = todaydate.getMonth() * 1;
             curday = todaydate.getDay() * 1;
@@ -177,7 +259,6 @@ function calendarSelectDate(day, month, year) {
     if (month < 10) {
         month = "0" + month;
     }
-    currentEditField.value = "" + day + month + year;
+    currentEditField.value = "" + day + "/" + month +  "/" + year;
     closeCurrentEditor();
 }
-
