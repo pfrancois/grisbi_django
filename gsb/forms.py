b5dbb84aa08a9f88f8dd22f4b2557785691a6cab
pfrancois(fixe) <pfrancois_99@yahoo.fr>
2011-07-05 00:19:10 +0200

    operationform

--- gsb/forms.py
+++ gsb/forms.py
@@ -2,10 +2,8 @@
 from django import forms
 from mysite.gsb.models import *
 from mysite.gsb import widgets
-
-class BaseForm(forms.ModelForm):
-    error_css_class = 'error'
-    required_css_class = 'required'
+import datetime
+import decimal
 
 class ImportForm(forms.Form):
     nom_du_fichier = forms.FileField()
@@ -17,9 +15,18 @@ class ImportForm(forms.Form):
     ('fusion','fusion des données avec le fichier')
     ))
 
+class BaseForm(forms.ModelForm):
+    error_css_class = 'error'
+    required_css_class = 'required'
+from django.core.exceptions import ValidationError
+def pointe_et_rapproche(value):
+    pass
 class OperationForm(BaseForm):
     compte=forms.ModelChoiceField(Compte.objects.all(),empty_label=None)
     cat=forms.ModelChoiceField(Cat.objects.all().order_by('type'),required=False)
+    montant=forms.DecimalField(localize=True,initial='0')
+    date=forms.DateField(input_formats=('%Y-%m-%d', '%d/%m/%Y', '%d/%m/%y','%d%m%y','%d%m%Y'),initial=datetime.date.today)
+    pointe=forms.BooleanField(required=False)
     mere=forms.ModelChoiceField(Ope.objects.filter(mere__isnull=False).order_by('-date'),required=False)
     class Meta:
         model=Ope
