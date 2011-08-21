d54939d2bc02ebeab45ce8c5c71a778dd90590b7
pfrancois <pfrancois_99@yahoo.fr>
2011-08-21 22:35:19 +0200

    button form inutile

--- gsb/templatetags/gsb_extras.py
+++ gsb/templatetags/gsb_extras.py
@@ -64,19 +64,3 @@ def dev(symbol=None):
     return symbol
 dev.is_safe = True
 
-@register.simple_tag
-def bouton_form(id_form, url, param=None):
-    '''
-    renvoie les boutons pour valider un formulaire
-    @param id_form:
-    @type id_form:
-    @param url:
-    @type url:chaine
-    @param param:
-    @type param:
-    '''
-    url=iri_to_uri(url)
-    if param:
-        return render_to_string('gsb/bouton_validation_formulaires.django.html', {'id_form':id_form, 'url_annul':url, 'url_param':param})
-    else:
-        return render_to_string('gsb/bouton_validation_formulaires.django.html', {'id_form':id_form, 'url_annul':url})
