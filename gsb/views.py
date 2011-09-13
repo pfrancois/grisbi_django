e2243bfd7e192e6ea5863a0462b9260f3e14a27a
pfrancois <pfrancois_99@yahoo.fr>
2011-09-14 01:04:20 +0200

    regression virement et ope

--- gsb/views.py
+++ gsb/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.shortcuts import render, get_object_or_404
-from mysite.gsb.models import Generalite, Compte, Ope, Compte_titre, Cat, Moyen, Titre
+from mysite.gsb.models import Generalite, Compte, Ope, Compte_titre, Moyen, Titre
 import datetime
 import mysite.gsb.forms as gsb_forms
 from django.db import models
