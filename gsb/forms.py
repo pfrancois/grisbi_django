311c5c86975b4728e17d7b0e9672f26dedc31dfe
pfrancois(fixe) <pfrancois_99@yahoo.fr>
2011-10-02 14:09:44 +0200

    ajout message erreur si pas de tiers dans le champ tiers

--- gsb/forms.py
+++ gsb/forms.py
@@ -144,6 +144,7 @@ class OperationForm(Basemodelform):
         data = self.cleaned_data
         if data['tiers'] is None:
             if not data['nouveau_tiers']:
+                self._errors['tiers'] = self.error_class(["si vous ne choisissez pas un tiers, vous devez taper le nom du nouveau dans le champs 'nouveau tiers'", ])
                 self._errors['nouveau_tiers'] = self.error_class(["si vous ne choisissez pas un tiers, vous devez taper le nom du nouveau", ])
                 del data['nouveau_tiers']
         if data['moyen'].type == u'd' and data['montant'] > 0:
