c8ede78f86649117262e32c847e7b162d7cb7ce5
pfrancois(fixe) <pfrancois_99@yahoo.fr>
2011-11-28 17:35:10 +0100

    refactorisation keepfilter

--- gsb/admin.py
+++ gsb/admin.py
@@ -59,9 +59,7 @@ class Modeladmin_perso(admin.ModelAdmin):
 
     fusionne_b_dans_a.short_description = u"fusion de 2 dans 1"
     #from here http://djangosnippets.org/snippets/2531/
-    def add_view(self, request, *args, **kwargs):
-        result = super(Modeladmin_perso, self).add_view(request, *args, **kwargs )
-
+    def keepfilter(self,request,result):
        # Look at the referer for a query string '^.*\?.*$'
         ref = request.META.get('HTTP_REFERER', '')
         if ref.find('?') != -1:
@@ -81,6 +79,9 @@ class Modeladmin_perso(admin.ModelAdmin):
             except:
                 pass
         return result
+    def add_view(self, request, *args, **kwargs):
+        result = super(Modeladmin_perso, self).add_view(request, *args, **kwargs )
+        return keepfilter(request,result)
     """
     Used to redirect users back to their filtered list of locations if there were any
     """
@@ -90,26 +91,11 @@ class Modeladmin_perso(admin.ModelAdmin):
         change_list after saving the page
         """
         result = super(Modeladmin_perso, self).change_view(request, object_id, extra_context )
+        return keepfilter(request,result)
 
-        # Look at the referer for a query string '^.*\?.*$'
-        ref = request.META.get('HTTP_REFERER', '')
-        if ref.find('?') != -1:
-            # We've got a query string, set the session value
-            request.session['filtered'] =  ref
-
-        if request.POST.has_key('_save'):
-            """
-            We only kick into action if we've saved and if
-            there is a session key of 'filtered', then we
-            delete the key.
-            """
-            try:
-                if request.session['filtered'] is not None:
-                    result['Location'] = request.session['filtered']
-                    request.session['filtered'] = None
-            except:
-                pass
-        return result
+    def delete_view(self, request, object_id, extra_context=None):
+        result = super(Modeladmin_perso, self).delete_view(self, request, object_id, extra_context)
+        return keepfilter(request,result)
 
 class Cat_admin(Modeladmin_perso):
     """classe admin pour les categories"""
