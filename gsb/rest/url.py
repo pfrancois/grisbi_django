ee001f56aa71f7e4dffc7ddcd8080eeb8d177b1d
pfrancois <pfrancois_99@yahoo.fr>
2015-01-25 20:14:28 +0100

    rest

--- gsb/rest/url.py
+++ gsb/rest/url.py
@@ -7,6 +7,14 @@ from . import view
 router = DefaultRouter()
 router.register(r'Tiers', view.TiersViewSet)
 router.register(r'Titres', view.TitreViewSet)
+router.register(r'Cours', view.CoursViewSet)
+router.register(r'Banques', view.BanqueViewSet)
+router.register(r'Cats', view.CatViewSet)
+router.register(r'Ibs', view.IbViewSet)
+router.register(r'Exercices', view.ExerciceViewSet)
+router.register(r'Comptes', view.CompteViewSet)
+router.register(r'Ope_Titres', view.OpeTitreViewSet)
+router.register(r'Ope', view.OpeViewSet)
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browseable API.
 urlpatterns = patterns('',
